94af7a9535559ebb878ea7eb33b5467ce924ff04
lgonzalez-EC <lgonzalez@edgecloud.com.mx>
2024-10-28 15:33:15 -0600

    Actualización en index

--- js/main.js
+++ js/main.js
@@ -88,8 +88,8 @@
         margin: 25,
         nav : true,
         navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
+            '<i class="bi bi-arrow-right"></i>',
+            '<i class="bi bi-arrow-left"></i>'
         ],
         responsiveClass: true,
         responsive: {

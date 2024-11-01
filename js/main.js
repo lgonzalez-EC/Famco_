813e6e8f00160f6cd0625d3c34225fd3e39b9940
Isaac Murillo <168232917+IsaacMur@users.noreply.github.com>
2024-11-01 15:34:56 -0600

    Modificaciones en detalle producto
    Modificaciones en detalle producto - diseño

--- js/main.js
+++ js/main.js
@@ -26,22 +26,25 @@
             } else {
                 $('.fixed-top').removeClass('shadow').css('top', 0);
             }
-        } 
+        }
     });
-    
-    
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+
+
+        // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 10) {
+            $('.back-to-top').fadeIn('fast'); 
+        } else {
+            $('.back-to-top').fadeOut('fast'); 
+        }
     });
+
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 450, 'easeInOutExpo');
+        $('html, body').scrollTop(0); // Desplazamiento instantáneo hacia arriba
         return false;
     });
+
+
 
 
     // Testimonial carousel
@@ -52,27 +55,27 @@
         dots: true,
         loop: true,
         margin: 25,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:1
+            576: {
+                items: 1
             },
-            768:{
-                items:1
+            768: {
+                items: 1
             },
-            992:{
-                items:2
+            992: {
+                items: 2
             },
-            1200:{
-                items:2
+            1200: {
+                items: 2
             }
         }
     });
@@ -86,27 +89,27 @@
         dots: true,
         loop: true,
         margin: 25,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-right"></i>',
             '<i class="bi bi-arrow-left"></i>'
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:1
+            576: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             },
-            1200:{
-                items:4
+            1200: {
+                items: 4
             }
         }
     });
